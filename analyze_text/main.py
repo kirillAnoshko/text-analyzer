3e2cb6579d7a8de53569a12e0f2b37bd81588488
kirillAnoshko <112968612+kirillAnoshko@users.noreply.github.com>
2023-06-22 19:49:29 +0300

    Add files via upload

--- analyze_text/main.py
+++ analyze_text/main.py
@@ -1,8 +1,12 @@
+import re
+
+
 class TextAnalyze:
     def __init__(self, file_name=None):
         if file_name is None:
             raise Exception("Не указан файл для анализа!")
         self.read_file(file_name)
+        self.check_empty_text()
         self.prepare_text()
         self.print_text()
 
@@ -14,15 +18,20 @@ class TextAnalyze:
                 self.text = self.file.read()  # здесь получается строка текста
         except FileNotFoundError:
             raise Exception(f"Файл {file_name} не найден!")
+
+    def check_empty_text(self):
         if not self.text:
-            raise Exception("Прочитанный файл пуст!")
+            raise RuntimeError(f"Файл {self.file} пуст, попробуйте другой файл")
 
     def prepare_text(self):
-        self.text = self.text.lower()        
-
+        self.text = self.text.lower()
+        self.clean_text = ''.join(re.findall(r'[\w\s-]', self.text))
+        self.words = self.text.split()
+        
     def print_text(self):
         """ выводит строку текста на экран """
-        print(self.text)
+        print(self.clean_text)
+        print(f"В этом тексте {len(self.words)} слов")
 
 
 TextAnalyze(file_name="text.txt")
